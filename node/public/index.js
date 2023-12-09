3867a66b0d4ad5628aec0892e5152b7021c3cca4
project-dy <kero122899@gmail.com>
2023-12-09 21:01:39 +0900

    Listing Second

--- node/public/index.js
+++ node/public/index.js
@@ -18,47 +18,84 @@ import '@material/web/button/text-button.js';
 import '@material/web/button/filled-tonal-button.js';
 import '@material/web/progress/circular-progress.js';*/
 import '@material/web/all';
+// import Toastify from 'toastify-js'
+
+document.getElementsByTagName('body')[0].style.display = 'block';
 
 window.isSelectedList = [];
 
-fetch('/api/list', {cache: "no-store"}).then((response) => {
-  return response.json();
-}).then((data) => {
-  listing(data);
-  check();
-});
-
-setInterval(() => {
-  fetch('/api/list', {cache: "no-store"}).then((response) => {
+function autoFetch () {
+  fetch('/api/list', { cache: "no-store" }).then((response) => {
     return response.json();
-  }).then((data) => {
+  })
+  .catch((err) => {
+    // console.error(err);
+    clearInterval(autoFetchInterval);
+    document.getElementById('err-vmFetch').setAttribute('open', 'true');
+    return null;
+  })
+  .then((data) => {
+    if (data == null) {
+      return;
+    }
     listing(data);
     check();
+    document.getElementById('refreshStatusIcon').outerHTML = '<md-icon slot="icon" id="refreshStatusIcon">refresh</md-icon>';
   });
-}, 1000);
+}
+autoFetch();
+
+window.autoFetchInterval = setInterval(() => {autoFetch()}, 400);
+
+window.autoFetch = autoFetch;
+let autoFetchInterval = window.autoFetchInterval;
 
 function listingInit(data) {
   console.log(data);
+  let button = document.createElement('md-text-button');
+  button.id = 'refreshStatus';
+  button.innerHTML = `<md-icon slot="icon" id="refreshStatusIcon">refresh</md-icon>Refresh Status`;
+  document.getElementById('info').innerHTML = '';
+  document.getElementById('info').appendChild(button);
+  button = document.getElementById('refreshStatus');
+  button.addEventListener('click', (event) => {
+    // document.getElementById('refreshStatusIcon').deleteAttribute('slot');
+    document.getElementById('refreshStatusIcon').outerHTML = '<md-circular-progress indeterminate id="refreshStatusIcon"></md-circular-progress>';
+    /*fetch('/api/list', { cache: "no-store" }).then((response) => {
+      return response.json();
+    }).then((data) => {
+      listing(data);
+      check();
+      // document.getElementById('refreshStatusIcon').setAttribute('slot', 'icon');
+      // document.getElementById('refreshStatusIcon').innerHTML = 'refresh';
+      document.getElementById('refreshStatusIcon').outerHTML = '<md-icon slot="icon" id="refreshStatusIcon">refresh</md-icon>';
+    });*/
+    window.autoFetchInterval = setInterval(() => {autoFetch()}, 400);
+  });
   const list = document.createElement('md-list');
   data.forEach((vm, index) => {
+    if (vm.domain.UUID == undefined || vm.domain.error) {
+      return;
+    }
     const div = document.createElement('div');
-    let state = '';
-    if (vm.domain.State === 'running') {
+    let state = '<md-circular-progress indeterminate></md-circular-progress>';
+    /*if (vm.domain.State === 'running') {
       state = `<md-icon slot="icon">devices</md-icon>`;
     } else if (vm.domain.State == 'shut off') {
       state = `<md-icon slot="icon">devices_off</md-icon>`;
-    }
-    console.log(vm);
+    }*/
+    // console.log(vm);
     /*if (window.isSelectedList[index] == undefined) {
       window.isSelectedList[index] = false;
     };*/
-    window.isSelectedList[index] = false;
+    // window.isSelectedList[index] = false;
     div.innerHTML = `
       <md-list-item index="${vm.domain.UUID}">
         <md-checkbox class="list-item-button"></md-checkbox>
         <span id="vmUUID">${vm.domain.UUID}</span>
         <span id="vmName">${vm.domain.Name}</span>
-        <span class="list-item-icon">${state}</span>
+        <span class="list-item-icon"><md-icon slot="icon"></md-icon></span>
+        <!--${state}-->
         <!--<md-switch></md-switch>-->
       </md-list-item>
       <md-divider></md-divider>
@@ -80,8 +117,7 @@ function listingInit(data) {
     div.appendChild(listItem);/**/
     list.appendChild(div);
   });
-  console.log(list);
-  document.getElementById('info').innerHTML = '';
+  // console.log(list);
   document.getElementById('info').appendChild(list);
   /*const applyButton = document.createElement('md-filled-button');
   applyButton.innerHTML = 'Apply';
@@ -99,37 +135,63 @@ function listing(data) {
       document.getElementById('vmChangedReload').setAttribute('open', 'true');
     }
 
-    Array.prototype.slice.call(listItems).forEach((listItem) => {
+    Array.prototype.slice.call(listItems).forEach((listItem, num) => {
       const index = listItem.getAttribute('index');
+      listItem.setAttribute('num', num);
       if (window.isSelectedList[index]) {
         listItem.getElementsByTagName('md-checkbox')[0].setAttribute('checked', 'true');
       } else {
         listItem.getElementsByTagName('md-checkbox')[0].removeAttribute('checked');
       }
-      console.log(data, index);
+      // console.log(data, index);
       //const state = listItem.getElementsByTagName('span')[2];
       // const vm = data[index];
       const vm = data.filter((vm) => {
         return vm.domain.UUID === index;
       })[0];
+
+      if (vm.domain.UUID == undefined || vm.domain.error) {
+        return;
+      }
+
       // console.log(vm);
       if (vm == undefined) {
+        document.getElementById('vmChangedReload').setAttribute('open', 'true');
         listItem.remove();
         return;
       }
+      // console.log(listItem);
       if (vm.domain.State === 'running') {
         //state.innerHTML = 'running';
         listItem.getElementsByTagName('md-icon')[0].innerHTML = 'devices';
       } else if (vm.domain.State == 'shut off') {
         //state.innerHTML = 'shut off';
         listItem.getElementsByTagName('md-icon')[0].innerHTML = 'devices_off';
+      } else if (vm.domain.State == 'paused') {
+        //state.innerHTML = 'paused';
+        listItem.getElementsByTagName('md-icon')[0].innerHTML = 'autopause';
+      } else if (vm.domain.State == 'crashed') {
+        //state.innerHTML = 'crashed';
+        listItem.getElementsByTagName('md-icon')[0].innerHTML = 'error';
+      } else if (vm.domain.State == 'pmsuspended') {
+        //state.innerHTML = 'pmsuspended';
+        listItem.getElementsByTagName('md-icon')[0].innerHTML = 'bedtime';
+      } else if (vm.domain.State == 'in shutdown') {
+        //state.innerHTML = 'inshutdown';
+        listItem.getElementsByTagName('md-icon')[0].innerHTML = 'settings_power';
+      } else if (vm.domain.State == 'idle') {
+        //state.innerHTML = 'idle';
+        listItem.getElementsByTagName('md-icon')[0].innerHTML = 'bed';
+      } else {
+        //state.innerHTML = 'unknown';
+        listItem.getElementsByTagName('md-icon')[0].innerHTML = 'question_mark';
       }
       // listItem.getElementsByTagName('span')[0].innerHTML = vm.domain.UUID;
       // listItem.getElementsByTagName('span')[1].innerHTML = vm.domain.Name;
-      console.log(listItem);
+      // console.log(listItem);
       listItem.querySelector('#vmUUID').innerHTML = vm.domain.UUID;
       listItem.querySelector('#vmName').innerHTML = vm.domain.Name;
-      console.log(index);
+      // console.log(index);
     });
   } else {
     listingInit(data);
@@ -139,18 +201,96 @@ function listing(data) {
 function check() {
   Array.prototype.slice.call(document.getElementsByTagName('md-checkbox')).forEach((checkbox) => {
     checkbox.addEventListener('change', (event) => {
-      console.log(checkbox.checked);
-      console.log(checkbox.parentNode.getAttribute('index'));
-      window.isSelectedList[checkbox.parentNode.getAttribute('index')] = checkbox.checked;
+      // console.log(checkbox.checked);
+      // console.log(checkbox.parentNode.getAttribute('index'));
+      // console.log(checkbox.parentNode.getAttribute('num'));
+      window.isSelectedList[checkbox.parentNode.getAttribute('num')] = [checkbox.parentNode.getAttribute('index'), checkbox.parentNode];
+      let selected = '';
+      window.isSelectedList.forEach((isSelected, index) => {
+        if (isSelected) {
+          // console.log(isSelected);
+          window.temp = isSelected;
+          selected += `<span class="list-item-icon"><md-icon slot="icon">devices</md-icon><span>${isSelected[1].getElementsByTagName('span')[1].innerHTML}<br> `;
+        }
+      });
+      document.getElementById('toolsConfirmDialog-Form-Selected-vmName').innerHTML = selected;
     });
   });
 }
 
 document.getElementById('tools').addEventListener('click', (event) => {
-  const tools = document.getElementById('tools');
+  const tools = document.getElementById('toolsDialog');
   if (tools.getAttribute('open') == 'true') {
     tools.setAttribute('open', 'false');
   } else {
     tools.setAttribute('open', 'true');
   }
+});
+
+Array.prototype.slice.call(document.getElementsByClassName('toolsDialog-Form-Radio')).forEach((radio) => {
+  radio.addEventListener('change', (event) => {
+    console.log(radio.value);
+    window.taskToPerform = radio.value;
+    window.taskToPerformRadio = radio;
+    document.getElementById('toolsConfirmDialog-Form-Task').innerHTML = radio.value;
+  });
+});
+
+document.getElementById('toolsConfirmDialog-Form-ConfirmButton').addEventListener('click', (event) => {
+  const taskToPerform = window.taskToPerform;
+  const isSelectedList = window.isSelectedList;
+  for (let i = 0; i < window.isSelectedList.length; i++) {
+    fetch(`/api/${window.taskToPerformRadio.getAttribute('apiPath').slice(' ').toLowerCase()}/${window.isSelectedList[i][0]}`, { cache: "no-store" }).then((response) => {
+      // console.log(response.json());
+      return response.text();
+    }).then((dataJSON) => {
+      // dataJSON.replaceAll(/'/g, '"');
+      dataJSON.replaceAll('\n', '\\n');
+      console.log(dataJSON.replaceAll('\n', '\\n'));
+      const data = JSON.parse(dataJSON.replaceAll('\n', '\\n'));
+      console.log(data);
+      console.log(1);
+      if (data.status == 'success') {
+        // document.getElementById('toolsConfirmDialog-Form-Result').innerHTML = 'Success';
+        Toastify({
+          text: `Success to perform ${taskToPerform} Task on ${isSelectedList[i][1].childNodes[5].innerHTML}!`,
+          duration: 3000,
+          // destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "right", // `left`, `center` or `right`
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+          style: {
+            // background: "linear-gradient(to right, #00b09b, #96c93d)",
+            background: "var(--md-filled-tonal-button-container-color, var(--md-sys-color-secondary-container, #e8def8))",
+            color: "var(--md-list-item-label-text-color, var(--md-sys-color-on-surface, #1d1b20))",
+          },
+          onClick: function(){} // Callback after click
+        }).showToast();
+      } else {
+        Toastify({
+          text: `Fail to perform ${taskToPerform} Task on ${isSelectedList[i][1].childNodes[5].innerHTML}!`,
+          duration: 3000,
+          // destination: "https://github.com/apvarun/toastify-js",
+          newWindow: true,
+          close: true,
+          gravity: "top", // `top` or `bottom`
+          position: "right", // `left`, `center` or `right`
+          stopOnFocus: true, // Prevents dismissing of toast on hover
+          style: {
+            // background: "linear-gradient(to right, #00b09b, #96c93d)",
+            background: "linear-gradient(to right, #ff416c, #ff4b2b)",
+          },
+          onClick: function(){} // Callback after click
+        }).showToast();
+        // document.getElementById('toolsConfirmDialog-Form-Result').innerHTML = 'Failed';
+      }
+      document.getElementById('toolsConfirmDialog-Form-Result').innerHTML += `<br>${data.message}`;
+      // document.getElementById('toolsConfirmDialog-Form-ConfirmButton').remove();
+    });
+  }
+
+  const toolsConfirmDialog = document.getElementById('toolsConfirmDialog');
+  toolsConfirmDialog.removeAttribute('open');
 });
--- node/src/index.js
+++ node/src/index.js
@@ -65,39 +65,128 @@ app.get('/api/list', (req, res) => {
     res.json(vms);
     res.end();
   });*/
-  exec('virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf list --all --uuid', (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      res.status(500).end();
-      return;
-    }
-    const lines = stdout.split('\n');
-    const vms = [];
-    for (let i = 0; i < lines.length - 2; i++) {
-      const line = lines[i];
-      const fields = line.split(/\s+/);
-      const info = execSync(`virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf dominfo ${fields[0]}`);
-      const infoLines = info.toString().split('\n');
-      // console.log(infoLines);
-      let dominfo = {};
-      infoLines.forEach((infoLine, index) => {
-        if (index > 0 && infoLine != '') {
-          const infoFields = infoLine.split(':');
-          dominfo[infoFields[0].trim()] = infoFields[1].trim();
+  try {
+    exec('virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf list --all --uuid', (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        res.status(500).end();
+        return;
+      }
+      const lines = stdout.split('\n');
+      const vms = [];
+      for (let i = 0; i < lines.length - 2; i++) {
+        const line = lines[i];
+        const fields = line.split(/\s+/);
+        let info;
+        try {
+          info = execSync(`virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf dominfo ${fields[0]}`);
+        } catch (err) {
+          info = err.stdout;
+          const vm = {
+            uuid: fields[0],
+            domain: {
+              error: info.toString(),
+            },
+          };
+          vms.push(vm);
+          continue;
         }
-      });
-      // console.log(dominfo);
+        const infoLines = info.toString().split('\n');
+        // console.log(infoLines);
+        let dominfo = {};
+        infoLines.forEach((infoLine, index) => {
+          if (index > 0 && infoLine != '') {
+            const infoFields = infoLine.split(':');
+            dominfo[infoFields[0].trim()] = infoFields[1].trim();
+          }
+        });
+        // console.log(dominfo);
 
-      const vm = {
-        uuid: fields[0],
-        domain: dominfo,
-      };
-      vms.push(vm);
-    }
-    res.json(vms);
+        const vm = {
+          uuid: fields[0],
+          domain: dominfo,
+        };
+        vms.push(vm);
+      }
+      res.json(vms);
+      res.end();
+    });
+  } catch (err) {
+    console.error(err);
+    // res.status(500).end();
+    res.send(`{"status": "error", "target": "list", "message": "${err.toString()}"}`);
     res.end();
-  });
+    return;
+  }
 });
+
+app.get('/api/start/:uuid', (req, res) => {
+  try {
+    exec(`virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf start ${req.params.uuid}`, (err, stdout, stderr) => {
+      if (err) {
+        // console.error(err);
+        // res.status(500).end();
+        // res.send(`{"status": "error", "target": "${req.params.uuid}", "message": "${stderr.trim()}"}`);
+        // res.end();
+        return err;
+      } else if (stderr) {
+        return stderr;
+      }
+      // res.send(stdout);
+      res.send(`{"status": "success", "target": "${req.params.uuid}", "message": "${stdout.trim()}"}`);
+      res.end();
+    });/*.catch((err) => {
+      console.error(err);
+      // res.status(500).end();
+      res.send(`{"status": "error", "target": "${req.params.uuid}", "message": "${err.toString()}"}`);
+      res.end();
+      return;
+    })*/
+  } catch (err) {
+    console.error(err);
+    // res.status(500).end();
+    res.send(`{"status": "error", "target": "${req.params.uuid}", "message": "${err.toString()}"}`);
+    res.end();
+    return;
+  }
+});
+
+app.get('/api/shutdown/:uuid', (req, res) => {
+  try {
+    exec(`virsh -c qemu:///system?authfile=/etc/ovirt-hosted-engine/virsh_auth.conf shutdown ${req.params.uuid}`, (err, stdout, stderr) => {
+      if (err) {
+        // console.error(err);
+        // res.status(500).end();
+        res.send(`{"status": "error", "target": "${req.params.uuid}", "message": "${stderr.trim()}"}`);
+        res.end();
+        return;
+      }
+      // res.send(stdout);
+      res.send(`{"status": "success", "target": "${req.params.uuid}", "message": "${stdout.trim()}"}`);
+      res.end();
+    });/*.catch((err) => {
+      console.error(err);
+      // res.status(500).end();
+      res.send(`{"status": "error", "target": "${req.params.uuid}", "message": "${err.toString()}"}`);
+      res.end();
+      return;
+    })*/
+  } catch (err) {
+    console.error(err);
+    // res.status(500).end();
+    res.send(`{"status": "error", "target": "${req.params.uuid}", "message": "${err.toString()}"}`);
+    res.end();
+    return;
+  }
+});
+
+// ex: http://localhost:3000/spice/spice_auto.html?host=localhost&port=5959
+
+app.get('/api/spice/:uuid', (req, res) => {
+  // TODO: check if needed websockify proccess's pid is exists from window.websockify_pid if not, return a location to start websockify
+});
+
+app.use('/spice', express.static('../spice-html5'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
